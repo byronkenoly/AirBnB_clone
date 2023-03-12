9a3b2e192d05ac6177368b4bfac12c6452ac8968
Byron Ochieng <byronochieng01@gmail.com>
2023-03-12 12:58:34 +0300

    task4

--- models/base_model.py
+++ models/base_model.py
@@ -18,10 +18,31 @@ class BaseModel:
         updated_at (datetime): time instance is updated
     """
 
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        """
+            __init__ assigns values when object of the class is created
+            *args: used to pass variable no. of non-key worded arguments
+            **kwargs: used to pass keyworded, variable length argument list
+            strptime() method creates datetime object from given string
+            
+            setattr() method is used to assign object attribute its value
+            syntax: setattr(obj, var, val)
+            obj: object whose which attribute is to be assigned
+            var: object attribute to be assigned
+            val: value with which var is to be assigned
+        """
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in ('created_at', 'updated_at'):
+                    if isinstance(value, str):
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if key != '__class__':
+                        setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return f'[<{self.__class__.__name__}>] ({self.id}) <{self.__dict__}>'
